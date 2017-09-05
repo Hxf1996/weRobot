7904f6e5020ee964b1058382f0d2da6ed746aaa4
huxiaofeng <huxiaofeng@aifocus.ai>
2017-09-05 15:53:23 +0800

    test

--- src/api/HTTP.js
+++ src/api/HTTP.js
@@ -2,12 +2,12 @@
 * @Author: 94078
 * @Date:   2017-04-30 15:20:09
 * @Last Modified by:   huxiaofeng
-* @Last Modified time: 2017-09-05 15:30:45
+* @Last Modified time: 2017-09-05 15:53:10
 */
 
 import Axios from 'axios';
 
-const baseURL = 'https://daily.elephtribe.com/procurement';
+const baseURL = 'http://daily.elephtribe.com/procurement';
 
 const HTTP = Axios.create({
     baseURL,
